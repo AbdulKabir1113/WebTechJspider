4c0f921b6cc30d85da386badbbc9a1bc3360dcb6
Abdul Kabir <abdulkabir0717@gmail.com>
2025-12-25 17:48:58 +0530

    BOM&DOM

---
+++ JS/Day15/bommethods.js
@@ -0,0 +1,25 @@
+// let a=window.prompt("enter ur name")
+// console.log(a);
+
+
+// document.writeln(a)
+
+// window.alert("this is alert method")
+
+// let x=window.confirm("confrm msg")
+// console.log(x)
+
+// window.console.error("dont sleep");
+// window.console.warn("ur waring msg")
+
+
+let a=window.Number(prompt("enter a table"))
+console.log(a);
+
+let b=window.Number(prompt("enter iteration"))
+console.log(b);
+
+for(var i=1; i<=b; i++){
+    document.writeln(`${b}*${i}=${b*i}<br>`)
+    // document.writeln("<br>")
+}
---
+++ JS/Day15/dommethods.js
@@ -0,0 +1,42 @@
+// write
+document.write("html")
+document.write("js")
+
+// writeln  provide space
+document.writeln("html")
+document.writeln("js")
+
+// body
+let a=document.body
+console.log(a);
+
+// head
+console.log(document.head);
+
+// title
+console.log(document.title);
+
+// URL
+console.log(document.URL);
+
+// baseURI
+console.log(document.baseURI);
+
+// froms
+console.log(document.forms);
+let b = document.forms
+console.log(b);
+console.log(b[1]);
+
+// images
+console.log(document.images);
+let c = document.images
+console.log(c);
+console.log(c[1]);
+
+
+// links
+console.log(document.links);
+let d = document.links
+console.log(d);
+console.log(d[0]);
